10c78acbc3bda430a429bf6c2ff7558c472059a2
Gerald Sandoval <sandovalgerald896@gmail.com>
2022-01-07 22:25:32 +0000

    Newest version running on pi. Need to fix buttons

--- catch_phrase.py
+++ catch_phrase.py
@@ -1,17 +1,52 @@
 
 import json
-from enum import Enum
+from signal import signal, SIGTERM, SIGHUP, pause
+import RPi.GPIO as GPIO 
+from rpi_lcd import LCD
+from time import sleep
 
 # COMICS, DISNEY, ENTERTAINMENT, EVERYTHING, FANTASY, HORROR, SCIFI, TECH
 
 
 class CatchPhrase(object):
     def __init__(self):
+        signal(SIGTERM, self.safe_exit)
+        signal(SIGHUP, self.safe_exit)
+        self.available_categories = {}
         self.phrases = {}
-        self.already_played = []
         self.team_one_score = 0
         self.team_two_score = 0
+        self.lcd = LCD(address=0x27)
+        sleep(1)
+        self.init_lcd()
+        self.init_gpio()
+        self.game_loop()
 
+    def safe_exit(self, signum, frame):
+        self.lcd.clear()
+        GPIO.cleanup() 
+        exit(1)
+
+    def init_lcd(self):
+        self.lcd.text("Hello!", 1)
+        self.lcd.text("Select Category!", 2)
+
+    def init_gpio(self):
+        GPIO.setwarnings(True)
+        GPIO.setmode(GPIO.BOARD)
+
+        self.init_button(gpio_pin=29, callback_func=self.skip)
+        self.init_button(gpio_pin=31, callback_func=self.choose_category)
+        self.init_button(gpio_pin=35, callback_func=self.add_point_team_one)
+        self.init_button(gpio_pin=37, callback_func=self.add_point_team_two)
+
+    def init_button(self, gpio_pin, callback_func):
+        GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(gpio_pin, GPIO.RISING, callback=callback_func)
+
+
+    '''
+    # TODO: manage phrase database/handling
     def read_game_database(self):
         with open("phrases/starwars.json") as json_phrases:
             self.add_phrases_to_game(phrases=json.load(json_phrases))
@@ -23,22 +58,40 @@ class CatchPhrase(object):
     def print_phrases(self):
         for phrase, categories in self.phrases.items():
             print(phrase)
+    '''
 
-    def skip(self):
+    def update_lcd(self, text):
+        #self.lcd.clear()
+        self.lcd.text(text[0:16], 1)
+        self.lcd.text(text[16:32], 2)
+
+    def skip(self, channel):
+        print("SKIP PRESSED")
+        #self.update_lcd("SKIP PRESSED")
+
+    def choose_category(self, channel):
+        print("CATEGORY PRESSED")
+        #self.update_lcd("CATEGORY PRESSED")
+
+    def add_point_team_one(self, channel):
+        print("TEAM ONE PRESSED")
+        #self.update_lcd("TEAM ONE PRESSED")
+
+    def add_point_team_two(self, channel):
+        print("TEAM TWO PRESSED")
+        #self.update_lcd("TEAM TWO PRESSED")
+
+    def start(self, channel):
         pass
 
-    def choose_category(self):
-        pass
-
-    def add_point(self, team):
-        # TODO: Say "TEAM X MY LORD", where x is the team number
-        pass
-
-    def start(self):
-        pass
+    def game_loop(self):
+        input("here")
+        #while True:
+        #    sleep(0.3)
 
 
 if __name__ == "__main__":
     catch_phrase = CatchPhrase()
-    catch_phrase.read_game_database()
-    catch_phrase.print_phrases()
+    #catch_phrase.read_game_database()
+    #catch_phrase.print_phrases()
+
